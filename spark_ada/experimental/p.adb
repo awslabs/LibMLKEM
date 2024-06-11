5f4f0f8034f93b287a912be5e6ffb2aff94f8a3c
Rod Chapman <rodchap@amazon.com>
2024-06-11 17:17:19 +0100

    Add constant-time Equals, Conditional_Copy and Condition_Unpad
    Signed-off-by: Rod Chapman <rodchap@amazon.com>

--- spark_ada/experimental/p.adb
+++ spark_ada/experimental/p.adb
@@ -1,6 +1,8 @@
 package body P
+  with SPARK_Mode => On
 is
    function Div1 (X : in Zq_Product) return U32
+     with SPARK_Mode => Off
    is
    begin
       --  Allow the compiler to implement as it sees fit.
@@ -14,6 +16,7 @@ is
    M : constant := (C / Q) + 1;
 
    function Div2 (X : in Zq_Product) return U32
+     with SPARK_Mode => Off
    is
       T : U64;
    begin
@@ -26,6 +29,7 @@ is
    end Div2;
 
    function Negate (X : in Zq) return Zq
+     with SPARK_Mode => Off
    is
       R : U32;
    begin
@@ -38,5 +42,60 @@ is
       --  This generates 6 instructions including the "ret" with same switches.
       --  return -X;
    end Negate;
+
+   function Equal (X, Y : in Byte_Seq) return Boolean
+   is
+      Arrays_Match : Boolean := True;
+      I : N32 := X'First;
+   begin
+      loop
+         Arrays_Match := Arrays_Match and (X (I) = Y (I));
+         pragma Loop_Invariant
+           (I >= X'First and I <= X'Last and
+            (Arrays_Match = (for all J in N32 range X'First .. I => X (J) = Y (J))));
+         exit when I = X'Last;
+         I := I + 1;
+      end loop;
+      return Arrays_Match;
+   end Equal;
+
+   --  Needs --level=1 or above to prove with CVC5
+   procedure Conditional_Copy (Dest      : in out Byte_Seq;
+                               Src       : in     Byte_Seq;
+                               Dont_Copy : in     Boolean)
+   is
+      Mask : constant U8 := 16#FF# * Boolean'Pos (not Dont_Copy);
+   begin
+      for I in Dest'Range loop
+         Dest (I) := Dest (I) xor ((Dest (I) xor Src (I)) and Mask);
+
+                  pragma Loop_Invariant (for all J in N32 range Dest'First .. I =>
+                                  Dest (J) = (if Dont_Copy then Dest'Loop_Entry (J) else Src (J)));
+      end loop;
+   end Conditional_Copy;
+
+   procedure Conditional_Unpad (Dest : in out Byte_Seq;
+                                Src  : in     Byte_Seq)
+   is
+      subtype Src_Slice is Byte_Seq (Dest'Range);
+
+      First_Padding_Byte_Index : constant N32 := 2;
+      First_Data_Byte_Index    : constant N32 := Src'Last - Dest'Last;
+      Zero_Byte_Index          : constant N32 := First_Data_Byte_Index - 1;
+      Last_Padding_Byte_Index  : constant N32 := Zero_Byte_Index - 1;
+      Dont_Copy                : U8;
+   begin
+      Dont_Copy := Src (0) or (Src (1) xor 16#02#);
+      Dont_Copy := Dont_Copy or Src (Zero_Byte_Index);
+
+      for I in N32 range First_Padding_Byte_Index .. Last_Padding_Byte_Index loop
+         pragma Loop_Optimize (No_Unroll, No_Vector);
+         Dont_Copy := Dont_Copy or Src (I);
+      end loop;
+
+      Conditional_Copy (Dest,
+                        Src_Slice (Src (First_Data_Byte_Index .. Src'Last)),
+                        Dont_Copy > 0);
+   end Conditional_Unpad;
 
 end P;
--- spark_ada/experimental/p.ads
+++ spark_ada/experimental/p.ads
@@ -1,5 +1,6 @@
 with Interfaces; use Interfaces;
 package P
+  with SPARK_Mode => On
 is
    Q : constant := 3329;
 
@@ -7,9 +8,13 @@ is
 
    subtype U64 is Unsigned_64;
    subtype U32 is Unsigned_32;
+   subtype N32 is Integer_32 range 0 .. Integer_32'Last;
    subtype U16 is Unsigned_16;
+   subtype U8 is Unsigned_8;
 
    subtype Zq_Product is U32 range 0 .. (Q - 1)**2;
+
+   type Byte_Seq is array (N32 range <>) of U8;
 
    function Div1 (X : in Zq_Product) return U32;
 
@@ -17,5 +22,38 @@ is
 
    function Negate (X : in Zq) return Zq
      with Post => (Negate'Result = -X);
+
+   function Equal (X, Y : in Byte_Seq) return Boolean
+     with Global => null,
+          Pre    => X'First = Y'First and
+                    X'Last  = Y'Last and
+                    X'Length >= 1 and
+                    Y'Length >= 1 and
+                    X'Length = Y'Length,
+          Post   => Equal'Result =
+                      (for all I in X'Range => X (I) = Y (I));
+
+   procedure Conditional_Copy (Dest      : in out Byte_Seq;
+                               Src       : in     Byte_Seq;
+                               Dont_Copy : in     Boolean)
+     with No_Inline,
+          Global => null,
+          Pre => Dest'First = Src'First and then
+                 Dest'Last  = Src'Last and then
+                 Dest'Length = Src'Length,
+          Post => (if Dont_Copy then Dest = Dest'Old else Dest = Src);
+
+   -- test cases Dest (0 .. 0), Src (0 .. 3)
+   procedure Conditional_Unpad (Dest : in out Byte_Seq;
+                                Src  : in     Byte_Seq)
+     with No_Inline,
+          Global => null,
+          Pre => Dest'First = 0 and then
+                 Src'First  = 0 and then
+                 Src'Last   >= 0 and then
+                 Dest'Last  >= 0 and then
+                 Src'Last   > Dest'Last and then
+                 Src'Length >= Dest'Length + 3 and then
+                 Src'Last - Dest'Last >= 3;
 
 end P;
