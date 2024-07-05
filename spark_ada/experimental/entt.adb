7c0869c1a2114b49925310a8ffb4a5cf12e02ad4
Rod Chapman <rodchap@amazon.com>
2024-07-05 14:16:47 +0100

    Add standard copyright notice
    Signed-off-by: Rod Chapman <rodchap@amazon.com>

--- spark_ada/experimental/entt.adb
+++ spark_ada/experimental/entt.adb
@@ -1,3 +1,6 @@
+--  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+--  SPDX-License-Identifier: Apache-2.0
+
 with Ada.Text_IO; use Ada.Text_IO;
 package body ENTT
   with SPARK_Mode => On
--- spark_ada/experimental/entt.ads
+++ spark_ada/experimental/entt.ads
@@ -1,3 +1,6 @@
+--  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+--  SPDX-License-Identifier: Apache-2.0
+
 with Interfaces; use Interfaces;
 package ENTT
   with SPARK_Mode => On
