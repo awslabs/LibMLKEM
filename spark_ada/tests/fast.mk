3602272662032bfb6f3bce0a06cb0248b886e307
Rod Chapman <rodchap@amazon.com>
2024-08-11 13:19:01 +0100

    Build with -O3 not -O2
    Signed-off-by: Rod Chapman <rodchap@amazon.com>

--- spark_ada/tests/fast.mk
+++ spark_ada/tests/fast.mk
@@ -2,4 +2,4 @@ include Makefile.common
 
 ../lib/libmlkem.a: ../src/*.ads ../src/*.adb ../libkeccak/lib/generic_none/libkeccak.a
 	@echo "--- Building LibMLKEM ---"
-	cd ..; gprclean -Pmlkem; gprbuild -Pmlkem -XMLKEM_BUILD_MODE=O2 -XMLKEM_RUNTIME_CHECKS=disabled -XMLKEM_CONTRACTS=disabled
+	cd ..; gprclean -Pmlkem; gprbuild -Pmlkem -XMLKEM_BUILD_MODE=O3 -XMLKEM_RUNTIME_CHECKS=disabled -XMLKEM_CONTRACTS=disabled
