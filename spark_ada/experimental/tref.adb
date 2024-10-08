c69a4cbca605c02b21457dbc3466674b6914f60c
Rod Chapman <rodchap@amazon.com>
2024-10-08 17:23:54 +0100

    Test C ref, Ada ref, and new Ada all at -O3
    Signed-off-by: Rod Chapman <rodchap@amazon.com>

--- spark_ada/experimental/tref.adb
+++ spark_ada/experimental/tref.adb
@@ -6,7 +6,7 @@ with Interfaces.C.Strings; use Interfaces.C.Strings;
 with cpucycles_h;
 procedure TRef
 is
-   P1, P2 : Poly_Zq;
+   P1, P2, P3 : Poly_Zq;
 
    procedure PP (F : in Poly_Zq)
    is
@@ -17,7 +17,7 @@ is
       New_Line (2);
    end PP;
 
-   Count1, Count2, Eref, Enew : Long_Long_Integer;
+   Count1, Count2, EAref, ECRef, Enew : Long_Long_Integer;
    I : chars_ptr;
 begin
    I := cpucycles_h.cpucycles_implementation;
@@ -35,31 +35,46 @@ begin
    PP (P1);
 
    P2 := P1;
+   P3 := P1;
 
+   -- Test and time C version
    Count1 := cpucycles_h.cpucycles.all;
    for I in 0 .. 999 loop
-      INTT  (P1);
+      CINTT  (P1);
    end loop;
    Count2 := cpucycles_h.cpucycles.all;
-   Eref := Count2 - Count1;
+   ECref := Count2 - Count1;
 
+   -- Test and time Ada reference code
    Count1 := cpucycles_h.cpucycles.all;
    for I in 0 .. 999 loop
-      INTTnew (P2);
+      INTT  (P2);
+   end loop;
+   Count2 := cpucycles_h.cpucycles.all;
+   EAref := Count2 - Count1;
+
+   -- Test and time new SPARK Code
+   Count1 := cpucycles_h.cpucycles.all;
+   for I in 0 .. 999 loop
+      INTTnew (P3);
    end loop;
    Count2 := cpucycles_h.cpucycles.all;
    Enew := Count2 - Count1;
 
 
-   Put_Line ("INTT(NTT(P1)):");
+   Put_Line ("INTT C:");
    PP (P1);
-   Put_Line ("INTT(P2):");
+   Put_Line ("INTT Ada ref:");
    PP (P2);
-   if P1 = P2 then
+   Put_Line ("INTT Ada new:");
+   PP (P3);
+
+   if P1 = P2 and P2 = P3 then
       Put_Line ("Pass");
    else
       Put_Line ("FAIL");
    end if;
-   Put_Line ("Ref Cycles = " & Eref'Img);
-   Put_Line ("New Cycles = " & Enew'Img);
+   Put_Line ("Ref   C Cycles = " & ECref'Img);
+   Put_Line ("Ref Ada Cycles = " & EAref'Img);
+   Put_Line ("New Ada Cycles = " & Enew'Img);
 end TRef;
