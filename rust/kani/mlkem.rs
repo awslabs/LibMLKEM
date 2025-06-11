bd0caa303bfce80e821de7fa7ebae0b21998940a
Rod Chapman <rodchap@amazon.com>
2025-06-11 13:16:43 +0100

    Add skeletal Rust modules for Kani and Verus
    Signed-off-by: Rod Chapman <rodchap@amazon.com>

---
+++ rust/kani/mlkem.rs
@@ -0,0 +1,12 @@
+// Copyright Kani Contributors
+// SPDX-License-Identifier: Apache-2.0 OR ISC OR MIT
+
+// Rules for Kani:
+//   1. Modular verification using contracts for all functions. No inlining!
+//   2. Unbounded verification using loop invariants. No loop unrolling!
+//   3. No test harness code - please use auto-harness-generation.
+//   4. All code must be "no_std" and "no_alloc" compatible.
+
+pub mod mlkem {
+
+}
