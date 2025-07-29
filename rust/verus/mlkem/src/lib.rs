55acb261814494c203c0ff382556dddb1918feaa
Rod Chapman <rodchap@amazon.com>
2025-07-29 16:11:48 +0100

    First attempt at code and proof of ntt_butterfly_block()
    Stuck on proof of loop invariant
    
    Signed-off-by: Rod Chapman <rodchap@amazon.com>

--- rust/verus/mlkem/src/lib.rs
+++ rust/verus/mlkem/src/lib.rs
@@ -2,6 +2,7 @@ use vstd::prelude::*;
 
 verus! {
 
+const N      : usize = 256;
 const Q      : i32 = 3329;
 const HALF_Q : i32 = 1665;
 const QINV   : u32 = 62209;
@@ -12,6 +13,8 @@ const U16_MAX_AS_I32 : i32 = u16::MAX as i32;
 const U16_MAX_AS_U32 : u32 = u16::MAX as u32;
 const I16_MIN_AS_I32 : i32 = i16::MIN as i32;
 const I16_MAX_AS_I32 : i32 = i16::MAX as i32;
+
+type Poly = [i16; N];
 
 fn montgomery_reduce (a : i32) -> (r : i16)
   requires -MRB < a < MRB
@@ -42,6 +45,45 @@ fn fqmul (a : i16, b : i16) -> (r : i16)
   let arg : i32 = (a as i32) * (b as i32);
   return montgomery_reduce (arg);
 }
+
+fn ntt_butterfly_block (r : &mut Poly, zeta : i16, start : usize, len : usize, bound : i16)
+  requires start < N,
+           1 <= len <= (N / 2),
+           start + 2 * len <= N,
+           0 <= bound < i16::MAX - Q as i16,
+           -HALF_Q < zeta < HALF_Q,
+           forall|i:int| 0 <= i < start ==> -(bound + Q) < old(r)[i],
+           forall|i:int| 0 <= i < start ==> old(r)[i] < bound + Q,
+           forall|i:int| start <= i < N ==> -bound < old(r)[i],
+           forall|i:int| start <= i < N ==> old(r)[i] < bound,
+  ensures  forall|i:int| 0 <= i < start + 2 * len ==> -(bound + Q) < r[i],
+           forall|i:int| 0 <= i < start + 2 * len ==> r[i] < bound + Q,
+           forall|i:int| start + 2 * len <= i < N ==> -bound < r[i],
+           forall|i:int| start + 2 * len <= i < N ==> r[i] < bound,
+{
+  for j in iter: start .. start + len // is range inclusive or exclusive upper bound?
+    invariant 0 <= j,
+              j < start + len,
+              start <= j < N,
+              0 <= start + len < N,
+              j + len < N,
+              0 <= bound < i16::MAX - Q as i16,
+              -HALF_Q < zeta < HALF_Q,
+              forall|i:int| 0 <= i < j ==> -(bound + Q) < r[i],
+              forall|i:int| 0 <= i < j ==> r[i] < bound + Q,
+              forall|i:int| j <= i < start + len ==> -bound < r[i],
+              forall|i:int| j <= i < start + len ==> r[i] < bound,
+              forall|i:int| start + len <= i < j + len ==> -(bound + Q) < r[i],
+              forall|i:int| start + len <= i < j + len ==> r[i] < bound + Q,
+              forall|i:int| j + len <= i < N ==> -bound < r[i],
+              forall|i:int| j + len <= i < N ==> r[i] < bound,
+  {
+     let t : i16 = fqmul(r[j + len], zeta);
+     r[j + len] = r[j] - t;
+     r[j]       = r[j] + t;
+  }
+}
+
 
 
 } // verus!
